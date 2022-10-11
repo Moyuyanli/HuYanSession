f6e1a810c745fbddbff85ce8b259f7306b484904
Moyuyanli <RoxyGreerad@163.com>
2022-10-11 16:15:10 +0800

    * 2.2.1 版本号变动

--- src/main/java/cn/chahuyun/HuYanSession.java
+++ src/main/java/cn/chahuyun/HuYanSession.java
@@ -33,7 +33,7 @@ public final class HuYanSession extends JavaPlugin {
     /**
      * 当前插件版本
      */
-    public static final String VERSION = "2.2.7";
+    public static final String VERSION = "2.2.1";
 
     private HuYanSession() {
         super(new JvmPluginDescriptionBuilder("cn.chahuyun.HuYanSession", VERSION)
