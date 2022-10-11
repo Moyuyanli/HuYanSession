55e6e0bbea616d1c460384209feb1a893d2c878f
Moyuyanli <RoxyGreerad@163.com>
2022-10-11 15:03:20 +0800

    * 2.2.7

---
+++ src/main/java/cn/chahuyun/manage/DataManager.java
@@ -0,0 +1,16 @@
+package cn.chahuyun.manage;
+
+/**
+ * 数据管理 导出 导入 excel
+ * 数据转移 所属 机器人的信息转移
+ *
+ * @author Moyuyanli
+ * @date 2022/10/11 14:57
+ */
+public class DataManager {
+
+    //todo 所属 机器人的信息转移
+
+    //todo 导出 导入 excel
+
+}
