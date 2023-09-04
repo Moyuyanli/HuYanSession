f7d3dee218825e02b89efac4b68ade6d3e63eccb
Moyuyanli <RoxyGreerad@163.com>
2023-09-04 15:23:19 +0800

    添加:通过参数查询对象方法

--- src/main/java/cn/chahuyun/session/utils/HibernateUtil.java
+++ src/main/java/cn/chahuyun/session/utils/HibernateUtil.java
@@ -2,7 +2,13 @@ package cn.chahuyun.session.utils;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaRoot;
 import xyz.cssxsh.mirai.hibernate.MiraiHibernateConfiguration;
+
+import java.util.List;
+import java.util.Map;
 
 import static cn.chahuyun.session.HuYanSession.LOGGER;
 
@@ -36,6 +42,51 @@ public class HibernateUtil {
             return;
         }
         LOGGER.info("H2数据库初始化成功!");
+    }
+
+    /**
+     * 通过参数从数据库中查询一个单实例
+     *
+     * @param params map参数集
+     * @param c      对象类
+     * @return T  单个实例
+     * @author Moyuyanli
+     * @date 2023/9/4 14:27
+     */
+    public static <T> T getSingleResult(Map<String, Object> params, Class<T> c) {
+        return HibernateUtil.factory.fromTransaction(session -> {
+            HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+            JpaCriteriaQuery<T> query = builder.createQuery(c);
+            JpaRoot<T> from = query.from(c);
+
+            query.select(from);
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.where(builder.equal(from.get(entry.getKey()), entry.getValue()));
+            }
+            return session.createQuery(query).getSingleResultOrNull();
+        });
+    }
+
+    /**
+     * 通过参数从数据库中查询所有实例
+     *
+     * @param params map参数集
+     * @param c      对象类
+     * @return List<T>  所有结果
+     * @author Moyuyanli
+     * @date 2023/9/4 14:27
+     */
+    public static <T> List<T> getList(Map<String, Object> params, Class<T> c) {
+        return HibernateUtil.factory.fromTransaction(session -> {
+            HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+            JpaCriteriaQuery<T> query = builder.createQuery(c);
+            JpaRoot<T> from = query.from(c);
+            query.select(from);
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.where(builder.equal(from.get(entry.getKey()), entry.getValue()));
+            }
+            return session.createQuery(query).list();
+        });
     }
 
 
